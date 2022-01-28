1d6179800bf238f2bdbc0e5a213678556d989238
LeoBzs <leobazacas@gmail.com>
2022-01-28 17:14:50 -0300

    Guia regras testado

--- src/test/java/TestCases/fazjogadacomsucesso.java
+++ src/test/java/TestCases/fazjogadacomsucesso.java
@@ -13,15 +13,42 @@ public class fazjogadacomsucesso extends TestBase {
     private final EntrarMesaTask entrarMesaTask = new EntrarMesaTask(driver);
     private final GuiaRapidoTask guiaRapidoTask = new GuiaRapidoTask(driver);
     private final RegrasTask regrasTask = new RegrasTask(driver);
+    private final DadoTask dadoTask = new DadoTask(driver);
+    private final CartasIndisponiveisTask cartasIndisponiveisTask = new CartasIndisponiveisTask(driver);
 
     @Test
-    public void ClicarSalaComSucesso(){
+    public void CaseGuiasComSucesso(){
         try{
             mainTask.clicaNaSala();
             criarMesaTask.realizarLogin();
             entrarMesaTask.clicaNaSala();
             guiaRapidoTask.clicaNoGuia();
             regrasTask.clicaRegras();
+            System.out.println("sucesso");
+        }catch (Exception e) {
+            System.out.println("Erro");
+        }
+    }
+    @Test
+    public void CaseDadosComSucesso(){
+        try{
+            mainTask.clicaNaSala();
+            criarMesaTask.realizarLogin();
+            entrarMesaTask.clicaNaSala();
+            dadoTask.jogarDado();
+            System.out.println("sucesso");
+        }catch (Exception e) {
+            System.out.println("Erro");
+        }
+    }
+
+    @Test
+    public void HoverParaCartaIndisponivel(){
+        try{
+            mainTask.clicaNaSala();
+            criarMesaTask.realizarLogin();
+            entrarMesaTask.clicaNaSala();
+            cartasIndisponiveisTask.hover();
             System.out.println("sucesso");
         }catch (Exception e) {
             System.out.println("Erro");
